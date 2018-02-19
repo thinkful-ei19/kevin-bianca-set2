e0b3ff7d8ac4380e369549dbd8f083f1d5525588
Kevin Murphy <midimurphdesigns@gmail.com>
2018-02-19 14:55:00 -0700

    setup switch statement with leapyear

---
+++ daysInAMonth.js
@@ -0,0 +1,108 @@
+function daysInAMonth(month, leapYear){
+    if (leapYear === false) {
+            switch( month )
+        {
+            case "January":
+            //
+            break;
+
+            case "February":
+            //
+            break;
+            
+            case "March":
+            //
+            break;
+            
+            case "April":
+            //
+            break;
+            
+            case "May":
+            //
+            break;
+            
+            case "June":
+            //
+            break;
+            
+            case "July":
+            //
+            break;
+            
+            case "August":
+            //
+            break;
+            
+            case "September":
+            //
+            break;
+            
+            case "October":
+            //
+            break;
+            
+            case "November":
+            //
+            break;
+            
+            case "December":
+            //
+            break;
+        }
+    } else {
+            switch( month )
+        {
+            case "January":
+            //
+            break;
+
+            case "February":
+            //
+            break;
+            
+            case "March":
+            //
+            break;
+            
+            case "April":
+            //
+            break;
+            
+            case "May":
+            //
+            break;
+            
+            case "June":
+            //
+            break;
+            
+            case "July":
+            //
+            break;
+            
+            case "August":
+            //
+            break;
+            
+            case "September":
+            //
+            break;
+            
+            case "October":
+            //
+            break;
+            
+            case "November":
+            //
+            break;
+            
+            case "December":
+            //
+            break;
+        }
+    }
+
+
+    
+}
